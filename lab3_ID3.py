5e23790462e73c2892a57cd4fa8690c62887cdba
nanjialin <nanjialin_2000@163.com>
2021-12-07 17:59:38 +0800

    finish lab3(ID3)

--- lab3_ID3.py
+++ lab3_ID3.py
@@ -1,128 +1,129 @@
-from __future__ import print_function
 import numpy as np
 import pandas as pd
+import json
 
 
-class TreeNode(object):
-    def __init__(self, ids=None, children=[], entropy=0, depth=0):
-        self.ids = ids  # index of data in this node
-        self.entropy = entropy  # entropy, will fill later
-        self.depth = depth  # distance to root node
-        self.split_attribute = None  # which attribute is chosen, it non-leaf
-        self.children = children  # list of its child nodes
-        self.order = None  # order of values of split_attribute in children
-        self.label = None  # label of node if it is a leaf
+class ID3Tree():
+    __count = 0
 
-    def set_properties(self, split_attribute, order):
-        self.split_attribute = split_attribute
-        self.order = order
+    def __init__(self):
+        super().__init__()
+        self.gain = {}
 
-    def set_label(self, label):
-        self.label = label
+    # 计算数据集的熵
+    def _entropy(self, dataSet):
+        labels = list(dataSet.columns)
+        level_count = dataSet[labels[-1]].value_counts().to_dict()  # 统计分类标签不同水平的值
+        entropy = 0.0
+        for key, value in level_count.items():
+            prob = float(value) / dataSet.shape[0]
+            entropy += -prob * np.log2(prob)
+        return entropy
+
+    # 获取子数据集
+    def _split_dataSet(self, dataSet, column, level):
+        subdata = dataSet[dataSet[column] == level]
+        del subdata[column]  # 删除这个划分字段列
+        return subdata.reset_index(drop=True)  # 重建索引
+
+    # 计算每个分类标签的信息增益，返回最大信息增益对应的标签
+    def _best_split(self, dataSet):
+        best_info_gain = 0.0    # 求最大信息增益
+        best_label = None       # 求最大信息增益对应的标签
+        labels = list(dataSet.columns)[: -1]   # 不包括最后一个靶标签
+        init_entropy = self._entropy(dataSet)  # 先求靶标签的香农熵
+        for _, label in enumerate(labels):
+            # 根据该label切割子数据集，并求熵
+            levels = dataSet[label].unique().tolist()  # 获取该分类标签的不同level
+            label_entropy = 0.0   # 用于累加各水平的信息熵；分类标签的信息熵等于该分类标签的各水平信息熵与其概率积的和。
+            # 遍历所有分类标签
+            for level in levels:
+                level_data = dataSet[dataSet[label] == level]  # 获取该水平的数据集
+                prob = level_data.shape[0] / dataSet.shape[0]  # 计算该水平的数据集在总数据集的占比
+                # 计算熵，并更新到label_entropy中
+                label_entropy += prob * self._entropy(level_data)  # _entropy用于计算香农熵
+
+            # 计算信息增益
+            info_gain = init_entropy - label_entropy
+
+            # 用best_info_gain来取info_gain的最大值，并获取对应的分类标签
+            if info_gain > best_info_gain:
+                best_info_gain = info_gain
+                best_label = label
+
+        self.__count += 1
+        return best_label
 
 
-def entropy(freq):
-    # remove prob 0
-    freq_0 = freq[np.array(freq).nonzero()[0]]
-    prob_0 = freq_0 / float(freq_0.sum())
-    return -np.sum(prob_0 * np.log(prob_0))
+    # 创建决策树
+    def mktree(self, dataSet):
+        target_list = dataSet.iloc[:, -1]
+        # 子样本集只有单个属性
+        if target_list.unique().shape[0] <= 1:
+            return target_list[0]
+        # 数据集只剩下把标签这一列数据；返回数量最多的水平
+        if dataSet.shape[1] == 1:
+            return self._top_amount_level(target_list)
 
+        # 不满足终止条件时，递归处理
+        best_label = self._best_split(dataSet)
+        #递归结束
 
-class DecisionTreeID3(object):
-    def __init__(self, max_depth=10, min_samples_split=2, min_gain=1e-4):
-        self.root = None
-        self.max_depth = max_depth
-        self.min_samples_split = min_samples_split
-        self.Ntrain = 0
-        self.min_gain = min_gain
+        best_label_levels = dataSet[best_label].unique().tolist()
+        tree = {best_label: {}}  # 生成字典，用于保存树状分类信息；这里不能用self.tree = {}存储
+        for level in best_label_levels:
+            level_subdata = self._split_dataSet(dataSet, best_label, level)  # 获取该水平的子数据集
+            tree[best_label][level] = self.mktree(level_subdata)  # 返回结果
+        return tree
 
-    def fit(self, data, target):
-        self.Ntrain = data.count()[0]
-        self.data = data
-        self.attributes = list(data)
-        self.target = target
-        self.labels = target.unique()
-
-        ids = range(self.Ntrain)
-        self.root = TreeNode(ids=ids, entropy=self._entropy(ids), depth=0)
-        queue = [self.root]
-        while queue:
-            node = queue.pop()
-            if node.depth < self.max_depth or node.entropy < self.min_gain:
-                node.children = self._split(node)
-                if not node.children:  # leaf node
-                    self._set_label(node)
-                queue += node.children
-            else:
-                self._set_label(node)
-
-    def _entropy(self, ids):
-        # calculate entropy of a node with index ids
-        if len(ids) == 0: return 0
-        ids = [i + 1 for i in ids]  # panda series index starts from 1
-        freq = np.array(self.target[ids].value_counts())
-        return entropy(freq)
-
-    def _set_label(self, node):
-        # find label for a node if it is a leaf
-        # simply chose by major voting
-        target_ids = [i + 1 for i in node.ids]  # target is a series variable
-        node.set_label(self.target[target_ids].mode()[0])  # most frequent label
-
-    def _split(self, node):
-        ids = node.ids
-        best_gain = 0
-        best_splits = []
-        best_attribute = None
-        order = None
-        sub_data = self.data.iloc[ids, :]
-        for i, att in enumerate(self.attributes):
-            values = self.data.iloc[ids, i].unique().tolist()
-            if len(values) == 1: continue  # entropy = 0
-            splits = []
-            for val in values:
-                sub_ids = sub_data.index[sub_data[att] == val].tolist()
-                splits.append([sub_id - 1 for sub_id in sub_ids])
-            # don't split if a node has too small number of points
-            if min(map(len, splits)) < self.min_samples_split: continue
-            # information gain
-            HxS = 0
-            for split in splits:
-                HxS += len(split) * self._entropy(split) / len(ids)
-            gain = node.entropy - HxS
-            if gain < self.min_gain: continue  # stop if small gain
-            if gain > best_gain:
-                best_gain = gain
-                best_splits = splits
-                best_attribute = att
-                order = values
-        node.set_properties(best_attribute, order)
-        child_nodes = [TreeNode(ids=split,
-                                entropy=self._entropy(split), depth=node.depth + 1) for split in best_splits]
-        return child_nodes
-
-    def predict(self, new_data):
+    def predict(self, tree, labels, test_sample):
         """
-        :param new_data: a new dataframe, each row is a datapoint
-        :return: predicted labels for each row
+        对单个样本进行分类
+        tree: 训练的字典
+        labels: 除去最后一列的其它字段
+        test_sample: 需要分类的一行记录数据
         """
-        npoints = new_data.count()[0]
-        labels = [None] * npoints
-        for n in range(npoints):
-            x = new_data.iloc[n, :]  # one point
-            # start from root and recursively travel if not meet a leaf
-            node = self.root
-            while node.children:
-                node = node.children[node.order.index(x[node.split_attribute])]
-            labels[n] = node.label
+        classLabel = None
+        firstStr = list(tree.keys())[0]  # tree字典里找到第一个用于分类键值对
+        secondDict = tree[firstStr]
+        featIndex = labels.index(firstStr)  # 找到第一个建(label)在给定label的索引
+        for key in secondDict.keys():
+            if test_sample[featIndex] == key:  # 找到test_sample在当前label下的值
+                if secondDict[key].__class__.__name__ == "dict":
+                    classLabel = self.predict(secondDict[key], labels, test_sample)
+                else:
+                    classLabel = secondDict[key]
+        return classLabel
 
-        return labels
+    def _unit_test(self):
+        data = [
+            ['晴', '炎热', '高', '弱', '取消'],  # 1
+            ['晴', '炎热', '高', '强', '取消'],  # 1
+            ['阴', '炎热', '高', '弱', '进行'],  # 1
+            ['雨', '适中', '高', '弱', '进行'],  # 1
+            ['雨', '寒冷', '正常', '弱', '进行'],  # 1
+            ['雨', '寒冷', '正常', '强', '取消'],  # 1
+            ['阴', '寒冷', '正常', '强', '进行'],  # 1
+            ['晴', '适中', '高', '弱', '取消'],  # 1
+            ['晴', '寒冷', '正常', '弱', '进行'],  # 1
+            ['雨', '适中', '正常', '弱', '进行'],  # 1
+            ['晴', '适中', '正常', '强', '进行'],  # 1
+            ['阴', '适中', '高', '强', '进行'],  # 1
+            ['阴', '炎热', '正常', '弱', '进行'],  # 1
+            ['雨', '适中', '高', '强', '取消'],  # 1
+        ]
+        data = pd.DataFrame(data=data, columns=['天气', '温度', '湿度', '风速', '活动'])
+        # 生成树
+        self.tree = self.mktree(data)
+        print(self.tree)
 
 
-if __name__ == "__main__":
-    df = pd.read_csv('weather.csv', index_col=0, parse_dates=True)
-    X = df.iloc[:, :-1]
-    y = df.iloc[:, -1]
-    tree = DecisionTreeID3(max_depth=3, min_samples_split=2)
-    tree.fit(X, y)
-    print(tree.predict(X))
+        # 测试样本
+        test_sample = ['晴', '炎热', '高', '若']
+        # 预测结果
+        outcome = self.predict(self.tree, ['天气', '温度', '湿度', '风速'], test_sample)
+        print("The ID3Tree pridicet the game will " + outcome)
+
+
+model = ID3Tree()
+model._unit_test()
